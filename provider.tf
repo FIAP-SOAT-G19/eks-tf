7ee60403b13f07a6452a89fd53a3b53913b05b42
Rafael Silva <rafaelsilvamap@gmail.com>
2024-03-13 19:33:36 -0300

    feat: add provider

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  backend "s3" {
+    bucket = "terraform-lab-fiap-1"
+    key    = "eks/terraform.tfstate"
+    region = "us-east-1"
+  }
+}
+
 provider "aws" {
   region = var.region
 }
